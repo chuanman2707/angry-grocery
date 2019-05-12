4112d34867e99ad76ba8634ba13fef34781906bb
chuanman2707 <chuanman2707@gmail.com>
2019-05-12 10:23:51 +0700

    update background

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
 import React from "react"
-import { Link } from "gatsby"
-import { FaBeer } from 'react-icons/fa'
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
@@ -13,8 +12,21 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
 
     </div>
-    <FaBeer />
   </Layout>
 )
+export const query = graphql`
+  {
+    img: file(relativePath: {
+      eq: "default-background.jpeg"
+    }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`;
+
 
 export default IndexPage
